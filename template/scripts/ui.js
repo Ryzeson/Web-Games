66e74d3f139e48ecde9f5e13a8c68c759cd70a9e
Ryzeson <ryzeson@gmail.com>
2024-03-20 21:29:08 -0400

    updated styling of game template

---
+++ template/scripts/ui.js
@@ -0,0 +1,8 @@
+// Toggles the hide/show arrows
+$(".collapse-controller").on("click", e => {
+    let arrowIcon = $(e.target);
+    if ($(e.target).children().length > 0 )
+        arrowIcon = $(e.target).children()[0];
+    $(arrowIcon).toggleClass("fa-caret-right");
+    $(arrowIcon).toggleClass("fa-caret-down");
+})
